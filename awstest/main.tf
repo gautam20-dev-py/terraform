c137c2c000f45792c891eceb46e7a2dcd7d9ff39
gautam20-dev-py <77130125+gautam20-dev-py@users.noreply.github.com>
2021-11-23 23:47:35 +0530

    Update main.tf

--- awstest/main.tf
+++ awstest/main.tf
@@ -2,3 +2,27 @@ provider "aws" {
   region = "ap-south-1"
   profile = "jenkins"
 }
+
+resource "aws_instance" "test_instance" {
+  ami = "amiid-ami-0108d6a82a783b352"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-54562f18"
+  associate_public_ip_address = "true"
+  security_groups = ["${aws_security_group.testinstance_sg.name}"]
+  key_name = "ec2KP"
+  tags {
+    Name = "testinstance"
+  }
+}
+
+resource "aws_security_group" "testinstance_sg" {
+name = "testinstance_sg"
+ingress{
+  description = "ssh"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+}
