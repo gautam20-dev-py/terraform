2f9e9190e32eef6d66fd2ec7d36fce9013dad092
gautam20-dev-py <77130125+gautam20-dev-py@users.noreply.github.com>
2021-11-25 00:01:13 +0530

    added s3

--- s3bucket/main.tf
+++ s3bucket/main.tf
@@ -3,6 +3,13 @@ provider "aws" {
   profile = "jenkins"
 }
 
-resource "aws_s3_bucket" "terraform_test_buck_1234"{
-
+resource "aws_s3_bucket" "test_bucket"{
+  name = "terraform_test_buck_1234"
+  acl = "private"
+  tags = {
+    name="terraform_test_buck_1234"
+    purpose="test"
+    environment = "test"
+  }
+  
 }
