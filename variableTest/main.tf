d4e9e5d3928aafb595b63e3d04fb7a7dab3a3dc2
gautam20-dev-py <77130125+gautam20-dev-py@users.noreply.github.com>
2021-11-24 23:08:36 +0530

    added multiline

--- variableTest/main.tf
+++ variableTest/main.tf
@@ -4,6 +4,19 @@ variable "name"{
     description = "this is a test variable"
 }
 
+variable "multiline"{
+    type = string
+    default = "Ankit"
+    description = <<EOF
+    this is a test variable
+    this is a test string
+    EOF
+}
+
 output "outputString" {
     value = "${var.name}"
+}
+
+output "outputmultiline" {
+    value = "${var.multiline}"
 }
