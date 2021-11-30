34d89355962287b9f2ac38f4a62f1606709cb37b
gautam20-dev-py <77130125+gautam20-dev-py@users.noreply.github.com>
2021-11-30 22:59:32 +0530

    added ec2 sg

---
+++ ec2Instance/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-south-1"
+  profile = "jenkins"
+}
+
+variable "vpc_id"{
+    type = "string"
+    default = "vpc-08a44663"
+}
+
+resource "aws_security_group" "terraform_allowssh" {
+    name = "terraform_allowssh"
+    description = "Security Group to allow SSh Access"
+    vpc_id = "${var.vpc_id}"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
