4b0a376b10ca190a1782262d5dfbdde3ecf63e46
gautam20-dev-py <77130125+gautam20-dev-py@users.noreply.github.com>
2021-11-30 23:38:22 +0530

    changed vpc

--- ec2Instance/main.tf
+++ ec2Instance/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 variable "vpc_id"{
     type = string
-    default = "vpc-094e3e333ecd8a755"
+    default = "vpc-f722e09c"
 }
 
 resource "aws_security_group" "terraform_allowssh" {
@@ -29,7 +29,7 @@ resource "aws_security_group" "terraform_allowssh" {
 resource "aws_instance" "test_instance" {
   ami = "ami-0108d6a82a783b352"
   instance_type = "t2.micro"
-  subnet_id = "subnet-0094b57eb446c551a"
+  subnet_id = "subnet-04035b48"
   associate_public_ip_address = "true"
   security_groups = ["${aws_security_group.terraform_allowssh.name}"]
   key_name = "ec2KP"
